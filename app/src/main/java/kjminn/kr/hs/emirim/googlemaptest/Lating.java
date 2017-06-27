166c49262c1937b013707c7efc01ea341b15a6e2
seungah <fofo2525@naver.com>
2017-06-27 18:48:41 +0900

    #2 callback Method

---
+++ app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/Lating.java
@@ -0,0 +1,8 @@
+package kjminn.kr.hs.emirim.googlemaptest;
+
+/**
+ * Created by appcreator13 on 2017. 6. 26..
+ */
+
+class Lating {
+}
--- app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -3,11 +3,33 @@ package kjminn.kr.hs.emirim.googlemaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.839269, 128.631892), 17));
+
+
     }
 }
